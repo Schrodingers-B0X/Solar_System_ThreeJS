235af9ee5bac1591d040b65f1afb9d40f279e381
TheCat <41850969+Schrodingers-B0X@users.noreply.github.com>
2024-12-01 22:39:02 +0000

    Restructure of files and dependencies.
    Changed the folder structure in production build to sort files into folders by file extension.
    
    Bundled three.js dependencies, textures and main script into /js/solar/.
    
    Turned off minify for readability of js in the production build.
    
    dist/
      |- css/
      |   !- solar.css
      |- js/
      |   !-solar/
      |      |-depends/
      |      |    |- react.js
      |      |    !- three.js
      |      !- solar.js
      |- img/
      |   !- planet.jpg
      !- index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), glsl()],
-})
+    build: {
+        minify: false,  // Disable minification
+        rollupOptions: {
+            input: {
+                index: './index.html',  // Correct the path as necessary
+            },
+            output: {
+                entryFileNames: `js/solar/[name].js`,
+                chunkFileNames: (chunkInfo) => {
+                    // Explicit naming based on chunk dependencies
+                    if (chunkInfo.name.startsWith('react') || chunkInfo.name.includes('fiber')) {
+                        return 'js/solar/depends/react.js';
+                    } else if (chunkInfo.name.includes('three')) {
+                        return 'js/solar/depends/three.js';
+                    }
+                    return `js/solar/[name].js`;
+                },
+                assetFileNames: ({ name, type }) => {
+					console.log({ name, type });
+                    if (name.endsWith('.css')) {
+                        // Use the original name for CSS files
+                        return `css/${name}`;
+                    }
+				return `${asset}/${name}`;
+                },
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('react') || id.includes('react-dom')) {
+							return 'react';
+						} else if (id.includes('three')) {
+							return 'three';
+						} else if (
+							id.includes('@react-three/fiber') ||
+							id.includes('@react-three/drei') ||
+							id.includes('@react-three/postprocessing') ||
+							id.includes('@react-three/rapier')
+						) {
+							return 'react-three';
+						}
+					}
+					return 'vendor';
+				}
+            },
+        },
+    },
+});
